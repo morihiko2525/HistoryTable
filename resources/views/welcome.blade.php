7cb25f85ca45b6b04e4524c032d6e4d3f6be88e5
Yuki Mori <morihiko223@gmail.com>
2021-04-28 07:08:24 +0000

    年表用のboxを追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,4 +5,12 @@
     <div class = top-img>
         <p class = title>Web年表作成</p>
     </div>
+    
+    <div class = "box">
+        <div>ここに写真を挿入する</div>
+        <div>2</div>
+        <div>3</div>
+    </div>
+    
+    
 @endsection
